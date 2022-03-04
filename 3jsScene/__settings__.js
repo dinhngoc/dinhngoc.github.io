99df53afd899a37aeba4a5691b3c5527d8229dc2
dinhngoc <6108158+dinhngoc@users.noreply.github.com>
2022-03-04 18:21:23 +0700

    Add files via upload

---
+++ 3jsScene/__settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1332731.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 68276440, 68276439, 69212678, 69212679 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/68276414/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/68276413/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/68276438/1/ammo.js', 'preload' : true},
+];
